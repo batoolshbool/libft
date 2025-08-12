ce32d8b535b73f0b62335af1a6734994bfa2b02f
batoolshbool <batoolmoe201@gmail.com>
2025-08-12 22:00:43 +0300

    mossajehad

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,39 +6,60 @@
 /*   By: bshbool <bshbool@student.42amman.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/11 14:40:18 by bshbool           #+#    #+#             */
-/*   Updated: 2025/08/12 21:02:43 by bshbool          ###   ########.fr       */
+/*   Updated: 2025/08/12 21:59:28 by bshbool          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
+#include <stdio.h>
+size_t	dig(int n)
+{
+	size_t	len;
+	
+	len = 0;
+	if (n < 0)
+	{
+		len++;
+		n *= -1;
+	}
+	while (n > 0)
+	{
+		n = n/10;
+		len++;
+	}
+	return (len);
+}
 char	*ft_itoa(int n)
 {
 	char	*str;
 	size_t	len;
-
-	len = ft_strlen(n);
-	str = (char *)malloc(sizeof(char) * n + 1);
+	size_t	i;
+	
+	str = NULL;
+	len = dig(n);
+	str = (char *)malloc(sizeof(char) * len + 1);
+	str[len] = '\0';
 	if (!str)
-		return (NULL);
+	return (NULL);
 	if (n < 0)
 	{
 		str[0] = '-';
-		n = -n;	
+		n *= -1;	
 	}
 	if (n == 0)
 		str[0] = '0';
+	i = len - 1;
 	while (n > 0)
 	{
-		str[len--] = (n % 10) + '0';
-		n = n/10;
+		str[i] = (n % 10) + '0';
+		n = (n / 10);
+		i--;
 	}
 	return (str);
+}
 
-}
-#include <stdio.h>
-int main()
-{
-	char *i = ft_itoa(55);
-	printf("%s\n", i);
-}
+//int main()
+//{
+//	char *i = ft_itoa(-42);
+//	printf("%s\n", i);
+//}
