d22fdf208bb281a12890696cb54e96babd8d989e
batoolshbool <batoolmoe201@gmail.com>
2025-09-02 15:22:22 +0300

    Sep 2 15:22

--- Makefile
+++ Makefile
@@ -2,6 +2,7 @@ NAME = libft.a
 CC = cc
 AR = ar rcs
 CFLAGS = -Wall -Wextra -Werror
+
 SRCS = ft_isalpha.c ft_isdigit.c ft_isalnum.c \
 	ft_isascii.c ft_isprint.c ft_strlen.c \
 	ft_memset.c ft_bzero.c ft_memcpy.c \
@@ -14,10 +15,10 @@ SRCS = ft_isalpha.c ft_isdigit.c ft_isalnum.c \
 	ft_itoa.c ft_strmapi.c ft_striteri.c \
 	ft_putchar_fd.c ft_putstr_fd.c ft_putendl_fd.c \
 	ft_putnbr_fd.c ft_split.c
-BONUS_SRCS = ft_lstnew_bonus.c ft_lstadd_front_bonus.c ft_lstsize_bonus.c \
-		ft_lstlast_bonus.c ft_lstadd_back_bonus.c ft_lstdelone_bonus.c \
-		ft_lstclear_bonus.c ft_lstmap_bonus.c ft_lstiter_bonus.c
 
+BONUS_SRCS = ft_lstnew_bonus.c ft_lstadd_front_bonus.c ft_lstsize_bonus.c \
+	ft_lstlast_bonus.c ft_lstadd_back_bonus.c ft_lstdelone_bonus.c \
+	ft_lstclear_bonus.c ft_lstmap_bonus.c ft_lstiter_bonus.c
 
 OBJS = $(SRCS:.c=.o)
 BONUS_OBJS = $(BONUS_SRCS:.c=.o)
@@ -25,20 +26,20 @@ BONUS_OBJS = $(BONUS_SRCS:.c=.o)
 %.o: %.c
 	$(CC) $(CFLAGS) -c $< -o $@
 
+all: $(NAME)
+
 $(NAME): $(OBJS)
 	$(AR) $(NAME) $(OBJS)
-
-all: $(NAME)
 
 bonus: $(OBJS) $(BONUS_OBJS)
 	$(AR) $(NAME) $(OBJS) $(BONUS_OBJS)
 
 clean:
-	rm -rf $(OBJS) $(BONUS_OBJS)
+	rm -f $(OBJS) $(BONUS_OBJS)
 
 fclean: clean
-	rm -rf $(NAME)
+	rm -f $(NAME)
 
 re: fclean all
 
-.PHONY = all clean fclean re bonus
+.PHONY: all clean fclean re bonus
--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: bshbool <bshbool@student.42amman.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/30 13:10:02 by bshbool           #+#    #+#             */
-/*   Updated: 2025/09/01 10:59:21 by bshbool          ###   ########.fr       */
+/*   Updated: 2025/09/02 15:16:41 by bshbool          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,78 +25,11 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 		temp = ft_lstnew(f(lst->content));
 		if (!temp)
 		{
-			ft_lstclear(&temp, del);
+			ft_lstclear(&new, del);
 			return (NULL);
-	
 		}
 		ft_lstadd_back(&new, temp);
 		lst = lst->next;
 	}
 	return (new);
 }
-
-/*
-Error in test 2: ft_lstmap(<list: {node: "hello!"}->(null)>, [s => __strlen(s)], [x => free(x)]) NULL check for 1th malloc: 
-Memory leak: 0x55cf725589f0 - 30 bytes
-You failed to free the memory allocated at:Most likely you are not calling del on the content when a new list node allocation fails.
-
-Error in test 3: ft_lstmap(<list: {node: "one"}->{node: "two"}->{node: "three"}->{node: "four"}->{node: "five"}->(null)>, [s => __strlen(s)], [x => free(x)]) NULL check for 1th malloc: 
-Memory leak: 0x55cf72558e70 - 30 bytes
-You failed to free the memory allocated at:Most likely you are not calling del on the content when a new list node allocation fails.
-
-Error in test 3: ft_lstmap(<list: {node: "one"}->{node: "two"}->{node: "three"}->{node: "four"}->{node: "five"}->(null)>, [s => __strlen(s)], [x => free(x)]) NULL check for 3th malloc: 
-Memory leak: 0x55cf72558ef0 - 30 bytes
-You failed to free the memory allocated at:
-Memory leak: 0x55cf72558f20 - 16 bytes
-You failed to free the memory allocated at:
-Memory leak: 0x55cf72558ec0 - 30 bytes
-You failed to free the memory allocated at:Most likely you are not calling del on the content when a new list node allocation fails.
-
-Error in test 3: ft_lstmap(<list: {node: "one"}->{node: "two"}->{node: "three"}->{node: "four"}->{node: "five"}->(null)>, [s => __strlen(s)], [x => free(x)]) NULL check for 5th malloc: 
-Memory leak: 0x55cf72558fc0 - 30 bytes
-You failed to free the memory allocated at:
-Memory leak: 0x55cf72558ff0 - 16 bytes
-You failed to free the memory allocated at:
-Memory leak: 0x55cf72558f70 - 30 bytes
-You failed to free the memory allocated at:
-Memory leak: 0x55cf72558fa0 - 16 bytes
-You failed to free the memory allocated at:
-Memory leak: 0x55cf72558e20 - 30 bytes
-You failed to free the memory allocated at:Most likely you are not calling del on the content when a new list node allocation fails.
-
-Error in test 3: ft_lstmap(<list: {node: "one"}->{node: "two"}->{node: "three"}->{node: "four"}->{node: "five"}->(null)>, [s => __strlen(s)], [x => free(x)]) NULL check for 7th malloc: 
-Memory leak: 0x55cf7255a4e0 - 30 bytes
-You failed to free the memory allocated at:
-Memory leak: 0x55cf7255a510 - 16 bytes
-You failed to free the memory allocated at:
-Memory leak: 0x55cf72559040 - 30 bytes
-You failed to free the memory allocated at:
-Memory leak: 0x55cf72559090 - 16 bytes
-You failed to free the memory allocated at:
-Memory leak: 0x55cf72559010 - 30 bytes
-You failed to free the memory allocated at:
-Memory leak: 0x55cf72559070 - 16 bytes
-You failed to free the memory allocated at:
-Memory leak: 0x55cf72558d80 - 30 bytes
-You failed to free the memory allocated at:Most likely you are not calling del on the content when a new list node allocation fails.
-
-Error in test 3: ft_lstmap(<list: {node: "one"}->{node: "two"}->{node: "three"}->{node: "four"}->{node: "five"}->(null)>, [s => __strlen(s)], [x => free(x)]) NULL check for 9th malloc: 
-Memory leak: 0x55cf7255a600 - 30 bytes
-You failed to free the memory allocated at:
-Memory leak: 0x55cf7255a680 - 16 bytes
-You failed to free the memory allocated at:
-Memory leak: 0x55cf7255a5b0 - 30 bytes
-You failed to free the memory allocated at:
-Memory leak: 0x55cf7255a630 - 16 bytes
-You failed to free the memory allocated at:
-Memory leak: 0x55cf7255a560 - 30 bytes
-You failed to free the memory allocated at:
-Memory leak: 0x55cf7255a5e0 - 16 bytes
-You failed to free the memory allocated at:
-Memory leak: 0x55cf7255a530 - 30 bytes
-You failed to free the memory allocated at:
-Memory leak: 0x55cf7255a590 - 16 bytes
-You failed to free the memory allocated at:
-Memory leak: 0x55cf7255a6a0 - 30 bytes
-You failed to free the memory allocated at:Most likely you are not calling del on the content when a new list node allocation fails.
-*/
